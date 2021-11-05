561e86b47c9e90d19975d27e0e4211f2ef186741
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:53:31 -0300

    Create  Triângulo_N_ Ordem_Crescente.py

---
+++ TheHuxley/3 - For/Triângulo_N_ Ordem_Crescente.py
@@ -0,0 +1,22 @@
+'''Faça um programa que dado um número inteiro n, imprima n linhas, onde cada linha deve seguir o seguinte padrão:
+
+1
+
+22
+
+333
+
+4444
+
+55555
+
+666666
+
+7777777
+
+…
+
+nnnnnnnnnn'''
+
+
+# Não fiz

6727375530b42a473308bc778ddadbc768f01831
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:48:01 -0300

    Create Impressão_de_tabuadas.py

---
+++ TheHuxley/3 - For/Impressão_de_tabuadas.py
@@ -0,0 +1,6 @@
+'''Faça um programa de resolução de tabuada. O programa deve inicialmente receber 2 números que indicam de quais números devem ser impressas a tabuada. 
+Por exemplo, se forem recebidos os valores 2 e 5, seu programa deve imprimir a tabuada de 2, 3, 4 e 5. O programa só deve aceitar valores entre 1 e 9. 
+Enquanto o usuário digitar valores que não sejam esses, emita uma mensagem de erro.'''
+
+
+# Não fiz

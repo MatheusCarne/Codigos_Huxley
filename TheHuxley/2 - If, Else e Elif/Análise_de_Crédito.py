157aeda2aeb0b674a56e18644b07c6a9312a7059
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:25:26 -0300

    Create  Análise_de_Crédito.py

---
+++ TheHuxley/2 - If, Else e Elif/Análise_de_Crédito.py
@@ -0,0 +1,7 @@
+'''Escreva um programa que determine qual é o valor mensal da parcela do empréstimo que um banco pode autorizar. 
+Para isso, o programa precisa do valor do salário e de quanto a renda mensal já está comprometida com outros empréstimos. 
+Considere que o limite de comprometimento do salário é de 30%. O programa deve calcular e mostrar na tela qual o limite disponível da parcela mensal do novo empréstimo.
+Fique atento porque a renda mensal comprometida já pode exceder os 30% e neste caso o limite disponível da parcela mensal será zero.'''
+
+
+# Não fiz

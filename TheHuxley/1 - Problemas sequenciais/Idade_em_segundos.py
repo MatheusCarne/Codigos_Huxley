40333b271556c247531c1a0535bf72e1fff7fd1d
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:19:14 -0300

    Create  Idade_em_segundos.py

---
+++ TheHuxley/1 - Problemas sequenciais/Idade_em_segundos.py
@@ -0,0 +1,7 @@
+'''Escreva um programa que leia a idade de uma pessoa e informe quantos segundos ela viveu.
+Obs: Considere que um ano sempre tem 365 dias.'''
+
+
+idade = int(input())
+ano = 86400 * 365
+print(idade * ano)

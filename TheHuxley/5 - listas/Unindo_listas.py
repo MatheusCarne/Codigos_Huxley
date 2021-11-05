207e48176e071bd61fbee8ebba2a9d158fefdce0
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:58:52 -0300

    Create Unindo_listas.py

---
+++ TheHuxley/5 - listas/Unindo_listas.py
@@ -0,0 +1,6 @@
+'''Faça um programa para ler duas listas de valores inteiros e gerar uma terceira lista que seja a união das duas listas informadas pelo usuário.
+
+As listas devem ter pelo menos 1 elemento. Caso contrário, deve ser exibida a mensagem "Erro - A lista deve ter pelo menos 1 elemento."'''
+
+
+# Não fiz

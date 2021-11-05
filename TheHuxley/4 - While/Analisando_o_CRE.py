05c5a509b092c28dd0298d242e34db13a75a0e95
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:57:15 -0300

    Create Analisando_o_CRE.py

---
+++ TheHuxley/4 - While/Analisando_o_CRE.py
@@ -0,0 +1,5 @@
+'''Fernanda tem um projeto de extensão e precisa selecionar alunos. 
+Escreva um programa para que ela possa informar matrícula e CRE dos vários inscritos, e ver, ao final, a matrícula do aluno com menor CRE e o CRE médio de todos os candidatos.'''
+
+
+# Não fiz

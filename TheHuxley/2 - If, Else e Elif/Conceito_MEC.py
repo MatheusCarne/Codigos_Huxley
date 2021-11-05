e0e6973782831f2a582ad9c411264cf61fb83f11
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:33:03 -0300

    Create  Conceito_MEC.py

---
+++ TheHuxley/2 - If, Else e Elif/Conceito_MEC.py
@@ -0,0 +1,9 @@
+'''Anualmente, o MEC avalia os cursos universitários de todo o país e atribui conceitos com base em diversos critérios, sendo um deles a quantidade de livros disponíveis. Considerando as regras definidas abaixo, escreva um programa que receba como entrada a quantidade de livros e a quantidade de alunos de um curso (nessa ordem) e exiba a letra maiúscula correspondente ao conceito concedido pelo MEC.
+
+1 livro para até 8 alunos --> Conceito A
+1 livro para entre 9 e 12 alunos --> Conceito B
+1 livro para entre 13 e 18 alunos --> Conceito C
+1 livro para mais de 18 alunos --> Conceito D'''
+
+
+# Não fiz

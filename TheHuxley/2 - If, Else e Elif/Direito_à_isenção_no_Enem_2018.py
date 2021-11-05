3c5a4ae91c2a8af8c76951f5984f8eb13ceaaf36
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:28:49 -0300

    Create Direito_à_isenção_no_Enem_2018.py

---
+++ TheHuxley/2 - If, Else e Elif/Direito_à_isenção_no_Enem_2018.py
@@ -0,0 +1,9 @@
+'''Em 2018, o Exame Nacional do Ensino Médio (Enem) comemora 20 anos com algumas novidades.
+Quem tem direito a não pagar a taxa de inscrição deverá fazer essa solicitação em um período anterior ao da inscrição.
+Quem pode pedir:
+inscritos no Exame Nacional para certificação de Competências de Jovens e Adultos (Encceja) 2017 que atingiram nota mínima (400 pontos).
+Estudantes que estão cursando a última serie do ensino médio em 2018 em escola da rede pública
+Concorrentes que cursaram todo o ensino médio em escola de rede publica ou como bolsista integral nas rede privada, e têm renda, por pessoa, igual ou menor que um salário mínimo e meio (R$ 1.431,00)'''
+
+
+# Não fiz

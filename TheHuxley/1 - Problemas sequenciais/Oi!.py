5d040157828f980e1a0c861fe873f9c8e67d3cf4
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:18:23 -0300

    Create  Oi!.py

---
+++ TheHuxley/1 - Problemas sequenciais/Oi!.py
@@ -0,0 +1,5 @@
+#Escreva um programa que leia o nome de uma pessoa e exiba a mensagem “Olá, FULANO” (onde FULANO é o nome da pessoa lida da entrada padrão).
+
+
+nome = input()
+print('Olah, {}!'.format(nome))

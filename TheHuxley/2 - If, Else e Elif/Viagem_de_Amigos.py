7e53f9ef5ff54502debb655868108cee3a396c63
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:35:27 -0300

    Create  Viagem_de_Amigos.py

---
+++ TheHuxley/2 - If, Else e Elif/Viagem_de_Amigos.py
@@ -0,0 +1,8 @@
+'''Luiza vai viajar com seus amigos e está pesquisando preços. O grupo está em dúvida entre alugar uma casa em Pipa ou em Fortaleza. 
+Caso escolham ir para Pipa, o aluguel de uma casa com 2 quartos sai por R$ 600 e o de uma com 3 quartos custa R$ 900. Lá eles pretendem fazer um passeio de barco, que custa R$ 75 por pessoa.
+Caso a escolha do grupo seja Fortaleza, eles poderão pagar R$ 950 por uma casa com 3 quartos ou R$ 1120 por uma com 4 quartos. A atração que querem visitar é um parque aquático cujo ingresso individual custa R$ 60.
+Escreva um programa que receba como entrada a quantidade de pessoas do grupo, a cidade escolhida, e a quantidade de quartos que querem na casa, e calcule e exiba o valor total da viagem, e quanto cada um irá pagar.
+Obs: Considere que todos do grupo irão para o passeio da cidade'''
+
+
+# Não fiz

47be11d24170ef4c55a8b67491ab8b985244bfb2
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:32:14 -0300

    Update Classificando_notas.py

--- TheHuxley/2 - If, Else e Elif/Classificando_notas.py
+++ TheHuxley/2 - If, Else e Elif/Classificando_notas.py
@@ -1,1 +1,9 @@
+'''Uma escola, ao divulgar a média dos seus alunos, decidiu incluir uma bolinha colorida ao lado da média. A regra ficou assim:
 
+notas maiores que 7, "bolinha verde";
+notas entre 5 e 7 (inclusive), "bolinha amarela";
+notas menores que 5, "bolinha vermelha".
+O seu programa, em Python, deve ler uma nota e classificá-la de acordo com a regra acima.'''
+
+
+# Não fiz

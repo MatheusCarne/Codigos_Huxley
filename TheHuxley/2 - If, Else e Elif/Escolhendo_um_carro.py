29e8ea0ec229a6e165f035062224ea88e28e84d4
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:26:35 -0300

    Create  Escolhendo_um_carro.py

---
+++ TheHuxley/2 - If, Else e Elif/Escolhendo_um_carro.py
@@ -0,0 +1,11 @@
+'''Faça um programa que verifique as condições de um carro e dê uma indicação à Henrique para a compra ou não do carro.
+
+Para ser considerado o carro deve atender a todos os requisitos obrigatórios. Feito isso, você deve verificar os requisitos desejáveis antes de imprimir uma mensagem.
+Se não atender os requisitos obrigatórios, imprima a mensagem "Não compre!"
+Caso ele atenda a todos os requisitos desejáveis, imprima na saída a mensagem "Pode comprar!"
+Caso ele atenda a apenas 2 requisitos desejáveis, imprima a mensagem "Boa compra."
+Caso ele atenda a apenas 1 requisito desejável, imprima a mensagem "Pode ser uma opção."
+Caso o carro não atenda nenhum requisito desejável, imprima a mensagem "Recomendo pensar melhor..."'''
+
+
+# Não fiz

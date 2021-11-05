a7a22c62d6f0ffdf81bfaadccb4fcd900dd7bcc3
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:52:18 -0300

    Create  Somando-Positivos_no_Intervalo.py

---
+++ TheHuxley/3 - For/Somando-Positivos_no_Intervalo.py
@@ -0,0 +1,6 @@
+'''Escreva um programa que receba como entrada dois números inteiros e retorne a soma dos números positivos no intervalo definido por eles, considerando inclusive os extremos.
+
+Obs: o intervalo pode ser crescente ou decrescente'''
+
+
+# Não fiz

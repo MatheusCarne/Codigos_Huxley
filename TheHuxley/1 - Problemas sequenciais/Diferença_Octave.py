94e13639536bdc1f14205142c3e98931438d2b44
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:20:05 -0300

    Create  Diferença_Octave.py

---
+++ TheHuxley/1 - Problemas sequenciais/Diferença_Octave.py
@@ -0,0 +1,9 @@
+#Leia quatro valores numéricos A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D.
+
+
+A = int(input('\nDIGITE O VALOR A:'))
+B = int(input('\nDIGITE O VALOR B:'))
+C = int(input('\nDIGITE O VALOR C:'))
+D = int(input('\nDIGITE O VALOR D:'))
+dif = (A * B) - (C * D)
+print('\nDIFERENCA = {}'.format(dif))

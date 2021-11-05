a1af6925597e9ae27ba24c26f3d8a71141e52b64
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:34:40 -0300

    Create Vingadores_defendendo_CG.py

---
+++ TheHuxley/2 - If, Else e Elif/Vingadores_defendendo_CG.py
@@ -0,0 +1,15 @@
+'''O vilão Thanos acredita que a salvação da Terra é acabar com metade da raça humana. Ele descobriu que existe uma cidade no Brasil onde existe o maior São João do Mundo e, por isso, achou que metade da raça humana estaria lá dançando forró. 
+Alguns Vingadores descobriram seus planos de atacar Campina e partiram pra lá, mas para conseguirem derrotar Thanos eles só podem atacá-lo com seu poder específico e se tiver energia suficiente para isso. 
+Seu objetivo é criar um algoritmo para informar ao departamento de defesa se o Vingador que está lutando no momento conseguiu ou não derrotar Thanos e salvar a Terra.
+
+Para isso você tem as seguintes informações:
+Vingador | Poder | Energia Mínima para Combate
+Homem de Ferro |  Armadura de Ferro | 10
+Hulk | Força Bruta | 5
+Capitão América | Escudo | 7
+Thor | Martelo | 4
+Gavião Arqueiro | Arco e Flecha | 12
+Viúva Negra | Inteligência | 20'''
+
+
+# Não fiz ta louco

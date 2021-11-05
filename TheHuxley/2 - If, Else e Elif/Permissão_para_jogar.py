3db5919d15aa5c0d0d5bf7b193c8c9e68bf1449e
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-05 18:39:29 -0300

    Create Permissão_para_jogar.py

---
+++ TheHuxley/2 - If, Else e Elif/Permissão_para_jogar.py
@@ -0,0 +1,5 @@
+'''Faça um programa que receba a idade do jogador e o tipo de jogo que ele deseja jogar e informe se ela pode jogar.
+Caso a idade do jogador caia fora do intervalo válido ou seja informado um tipo de jogo que não existe, deve ser emitida uma mensagem de erro de acordo com o que aconteceu.'''
+
+
+# Não fiz

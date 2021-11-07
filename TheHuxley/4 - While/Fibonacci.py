cf672cf2c1f6abb0e9a984e3a122c71804c1fbac
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 15:49:52 -0300

    Create  Fibonacci.py

---
+++ TheHuxley/4 - While/Fibonacci.py
@@ -0,0 +1,6 @@
+'''A sequência de Fibonacci é definida pela seguinte sequência: 0 1 1 2 3 5 8 13 21... 
+Em que cada termo da sequência é obtido somando os dois termos anteriores.
+Escreva um programa que imprima os n primeiros termos da sequência dos números Fibonacci.'''
+
+
+# Não fiz

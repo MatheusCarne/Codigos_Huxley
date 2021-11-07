dd9007c0154220b3eb230b35375933e2c41799b3
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 16:20:51 -0300

    Create  Resumo_d_fig_geométricas.py

---
+++ TheHuxley/8 - Funções/Resumo_d_fig_geométricas.py
@@ -0,0 +1,9 @@
+'''Escreva um programa que recebe informações de várias figuras geométricas  e, em seguida, imprime um resumo das características (área e comprimento) dessas figuras. 
+O programa pode receber até 3 tipos de figuras: quadrado, retângulo e círculo, identificados pela primeira letra da figura. 
+Caso as dimensões da figura forem negativas, o resultado do cálculo das características será -1. O programa encerrará quando o usuário digitar sair no tipo da figura.
+Use funções para modularizar seu programa. 
+Faça uma função para cada cálculo de característica de uma figura. Faça também uma função para imprimir o resumo final.
+Obs.: Use pi = 3.14 e o resultado com 2 casas decimais de aproximação.'''
+
+
+# Não fiz

66aeada040fd240816c95d9f7c2cb5a036615b54
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 15:58:44 -0300

    Create exibindo_nota_posição.py

---
+++ TheHuxley/5 - listas/exibindo_nota_posição.py
@@ -0,0 +1,6 @@
+'''O objetivo dessa tarefa é aprender a manipular listas em Python. 
+Deve-se apresentar um programa que leia até 5 notas de um determinado aluno e as guarde em uma lista.
+Em seguida, o programa deve exibir cada nota lida indicando sua posição na lista.'''
+
+
+# Não fiz

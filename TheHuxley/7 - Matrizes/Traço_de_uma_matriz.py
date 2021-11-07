72a03167c7ed6b0bfb20eded39b60191fa386f46
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 16:15:21 -0300

    Create  Traço_de_uma_matriz.py

---
+++ TheHuxley/7 - Matrizes/Traço_de_uma_matriz.py
@@ -0,0 +1,7 @@
+'''Na álgebra linear, o traço de uma matriz quadrada é a função matricial que associa a matriz à soma dos elementos da sua diagonal principal.
+(https://pt.wikipedia.org/wiki/Tra%C3%A7o_(%C3%A1lgebra_linear))
+traço(A) = a11 + a22 + ... + ann
+Dada uma matriz quadrada de ordem n, calcule o traço dessa matriz.'''
+
+
+# Não fiz

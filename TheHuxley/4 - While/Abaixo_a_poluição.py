e8d42dbd04418cce13670d8a5d0dfa16d4577a44
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 15:47:25 -0300

    Create Abaixo_a_poluição.py

---
+++ TheHuxley/4 - While/Abaixo_a_poluição.py
@@ -0,0 +1,6 @@
+'''Devido aos altos índices de poluição, uma cidade resolveu multar as casas que possuem mais de 2 veículos em R$ 12.89 por mês por cada veículo extra. 
+Escreva um programa que receba como entrada a quantidade de veículos de várias residências, até que seja informado o valor 999, 
+e exiba o total mensal arrecadado com as multas, e a quantidade de casas multadas.'''
+
+
+# Não fiz

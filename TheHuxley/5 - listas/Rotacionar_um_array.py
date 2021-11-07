003a8856a601c5c42e0b69f8e9cf6ab37037a3c7
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 16:06:05 -0300

    Create  Rotacionar_um_array.py

---
+++ TheHuxley/5 - listas/Rotacionar_um_array.py
@@ -0,0 +1,13 @@
+'''Você receberá um array de inteiros e deve realizar deslocamentos à esquerda.
+Por exemplo, o array:
+
+1 2 3 4 5
+
+Com dois deslocamentos à esquerda, ficaria:
+
+3 4 5 1 2
+
+Perceba que todos os elementos foram deslocados de duas posições.'''
+
+
+# Não fiz

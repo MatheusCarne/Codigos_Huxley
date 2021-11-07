70e0d4e9ef4a30a1c26059ec3ff8c1299fdd5ad8
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 16:01:06 -0300

    Create  Menor_Valor_Posição.py

---
+++ TheHuxley/5 - listas/Menor_Valor_Posição.py
@@ -0,0 +1,7 @@
+'''Escrever um programa que lê um número N. 
+Este N é o tamanho de um array.
+Em seguida, leia cada um dos N números do array, encontre o menor elemento desse array, a sua posição dentro do array e imprima essas informações.
+Considere que o array começa na posição 0'''
+
+
+# Não fiz

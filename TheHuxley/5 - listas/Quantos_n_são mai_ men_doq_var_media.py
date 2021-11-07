5c53a5ea3bccced7992ca384e4066ba99c0db406
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 16:02:32 -0300

    Create  Quantos_n_são mai_ men_doq_var_media.py

---
+++ TheHuxley/5 - listas/Quantos_n_são mai_ men_doq_var_media.py
@@ -0,0 +1,5 @@
+'''Serão dados n números correspondendo as notas da turma de alunos de uma escola. 
+Escreva um programa que leia essas notas e calcule quantas estão mais de 10% acima da média e quantas estão menos de 10% abaixo da média.'''
+
+
+# Não fiz

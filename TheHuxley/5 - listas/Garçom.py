dada0fc7ea1eb22b2b8b5c76cc3a6d0a39b6e66e
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 16:05:20 -0300

    Create  Garçom.py

---
+++ TheHuxley/5 - listas/Garçom.py
@@ -0,0 +1,8 @@
+'''Parte do treinamento de um novo garçom é carregar uma grande bandeja com várias latas de bebidas e copos e entregá-las todas numa mesa do restaurante. 
+Durante o treinamento é comum que os garçons deixem cair as bandejas, quebrando todos os copos. 
+A SBC — Sociedade Brasileira de Copos — analisou estatísticas do treinamento de diversos garçons e descobriu que os garçons em treinamento deixam cair apenas bandejas que têm mais latas de bebidas que copos. 
+Por exemplo, se uma bandeja tiver 10 latas e 4 copos, certamente o garçom em treinamento a deixará cair, quebrando os 4 copos. Já se a bandeja tiver 5 latas e 6 copos, ele conseguirá entregá-la sem deixar cair.
+Escreva um programa que, dado o número de latas e copos em cada bandeja que o garçom tentou entregar, imprime o total de copos que ele quebrou'''
+
+
+# Não fiz

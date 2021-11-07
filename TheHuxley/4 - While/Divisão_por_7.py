342b0338da0b2a9867585f2236f468c2e5ac8208
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 15:48:31 -0300

    Create  Divisão_por_7.py

---
+++ TheHuxley/4 - While/Divisão_por_7.py
@@ -0,0 +1,8 @@
+'''Seven é uma menina muito curiosa e gosta muito das aulas de matemática. 
+A professora passou uma atividade para saber se um número é divisível por 7, porém os números eram muito grandes e ela queria uma outra forma de determinar isto. 
+Após algum tempo pensando, descobriu que se pegar o último dígito de qualquer número inteiro, multiplicar por 5 e adicionar à parte restante do número (sem o último dígito), obterá um novo número. 
+Se este número for divisível por 7, o número original também é. 
+Agora ela precisa de sua ajuda para implementar este algoritmo seguindo seu raciocínio e ajudar na atividade de casa.'''
+
+
+# Não fiz

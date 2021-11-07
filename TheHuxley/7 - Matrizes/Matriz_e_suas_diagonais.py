50b539f110d96e7dc5ad86a7d143626bf732471c
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 16:16:02 -0300

    Create  Matriz_e_suas_diagonais.py

---
+++ TheHuxley/7 - Matrizes/Matriz_e_suas_diagonais.py
@@ -0,0 +1,5 @@
+'''Faça um programa para gerar uma matriz de inteiros N x N e preenche-la com inteiros sequencias de 1 a N*N. 
+Em seguida o programa deve exibir a matriz, a sua diagonal principal e a secundária'''
+
+
+# Não fiz 

851abba8efb161d440ce885d6d61fc594282f90a
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 16:13:19 -0300

    Create  Elementos_de_uma_matriz.py

---
+++ TheHuxley/7 - Matrizes/Elementos_de_uma_matriz.py
@@ -0,0 +1,6 @@
+'''Com o aumento do uso dos computadores a teoria das matrizes está cada vez mais sendo aplicada em áreas como, Engenharia, Matemática, dentre outras.
+Assim o desafio é você criar um programa que peça ao usuário a quantidade de linhas e colunas de uma matriz que ele quer. 
+Em seguida, inserir os elementos da matriz e imprimir a saída esperada.'''
+
+
+# Não fiz

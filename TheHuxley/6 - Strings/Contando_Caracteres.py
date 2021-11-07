3d802f1f21efe9ac7f803d14389e068c941af802
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 16:10:50 -0300

    Create  Contando_Caracteres.py

---
+++ TheHuxley/6 - Strings/Contando_Caracteres.py
@@ -0,0 +1,12 @@
+'''Matheus estava conversando com a sua noiva via mensagem de texto, quando ela lhe enviou a seguinte mensagem:
+
+1-4-3
+
+Ele não entendeu a mensagem, então ele perguntou o que isso significava, 
+e ela respondeu que era 'I Love You" e logo ele percebeu que cada número separado por um ' - ' 
+é a quantidade de caracteres de cada uma das palavras que compõem a frase. 
+Com isso, ele teve a ideia de criar um programa que inserindo determinada frase, ele calcula a quantidade de caracteres de cada uma das palavras e separa os valores por ' - '. 
+Mas ele ainda teve a ideia de que o programa deveria receber várias frases linha por linha e ainda no final da execução do programa, a palavra com a maior quantidade de letras deveria ser exibida.'''
+
+
+# Não fiz 

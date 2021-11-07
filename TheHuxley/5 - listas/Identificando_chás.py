f71b7abf1607c8a15d95c9f0b402618b163bcf2b
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 16:04:21 -0300

    Create  Identificando_chás.py

---
+++ TheHuxley/5 - listas/Identificando_chás.py
@@ -0,0 +1,9 @@
+'''Degustação de chá às cegas é a habilidade de identificar o chá usando somente os sentidos do paladar e do olfato.
+Como parte do Incrível Campeonato de Puristas bebedores de Chá (ICPC), um programa de TV local organizou uma degustação. 
+Durante o programa, cada competidor recebe uma chícara do mesmo chá. 
+Eles podem cheirar e experimentar para tentar identificar o tipo de chá, que pode ser: (1) chá-branco; (2) chá-verde; (3) chá-preto; ou (4) chá de ervas. 
+Ao final, as respostas são verificadas para saber o número de respostas corretas.
+Dado o tipo de chá atual e as respostas dos participantes, determine a quantidade de competidores que acertaram a resposta'''
+
+
+# Não fiz

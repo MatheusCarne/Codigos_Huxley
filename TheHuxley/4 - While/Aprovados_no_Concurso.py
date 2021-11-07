abc28e4a7fdf99d74516a1b0630cd037afc25497
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 15:53:05 -0300

    Create  Aprovados_no_Concurso.py

---
+++ TheHuxley/4 - While/Aprovados_no_Concurso.py
@@ -0,0 +1,7 @@
+'''O IBGE realizou um concurso para contratar pessoas para trabalhar no censo. 
+Cada candidato fez uma prova de português com 50 questões, outra de matemática com 35 questões, e uma prova de redação.
+Para ser aprovado, era necessário acertar pelo menos 80% da prova de português, 60% da prova de matemática, e ter nota igual ou superior a 7 na redação.
+Escreva um programa que receba como entrada, para cada candidato, a quantidade de questões certas em português e em matemática, e também a nota na redação, e depois exiba quantos candidatos foram aprovados.'''
+
+
+# Não fiz

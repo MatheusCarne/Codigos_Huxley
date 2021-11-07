253ea511f8a6b2b0d1724b13c84e719a69e882f1
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 15:51:36 -0300

    Create  Monitorando_A_Glicose.py

---
+++ TheHuxley/4 - While/Monitorando_A_Glicose.py
@@ -0,0 +1,8 @@
+'''Genival tem muito medo de ter diabetes e, por isso, o médico pediu que ele medisse sua glicose ao longo do dia para ver se ela estava controlada.
+Escreva um programa que receba como entrada os vários valores da taxa de glicose de Genival, até que o valor informado seja 0, e calcule a glicose média observada naquele dia.
+Caso esse valor seja inferior a 110, o programa deve exibir a mensagem Glicose Normal. 
+Caso seja 200 ou mais, a mensagem exibida deve ser Glicose Muito Alta. 
+Nos demais casos, o programa deve exibir Glicose Alterada'''
+
+
+# Não fiz

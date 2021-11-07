f4baa5c1d1fc49ee78ed7c3a42c6059900ce217d
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 15:54:35 -0300

    Create  É_um_número_triangular?.py

---
+++ TheHuxley/4 - While/É_um_número_triangular?.py
@@ -0,0 +1,6 @@
+'''Um número natural é  triangular se ele é produto de três números naturais consecutivos. 
+Por exemplo: 120 é triangular, pois 4*5*6 = 120. O número 2730 é triangular, pois 13*14*15 = 2730. 
+Dado um número natural (inteiro não-negativo) n, verifique se ele é triangular ou não. '''
+
+
+# Não fiz

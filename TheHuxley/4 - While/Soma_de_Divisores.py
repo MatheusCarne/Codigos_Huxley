c3c45ee68e0bf6eda5a1ef99a3b38901b44d1b3e
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 15:53:45 -0300

    Create  Soma_de_Divisores.py

---
+++ TheHuxley/4 - While/Soma_de_Divisores.py
@@ -0,0 +1,5 @@
+'''Escreva um programa que receba como entrada 5 números e exiba aquele cuja soma dos divisores é a maior.
+Ex: A soma dos divisores de 4 é 7 (1 + 2 + 4), enquanto a soma dos divisores de 5 é 6 (1 + 5)'''
+
+
+# Não fiz

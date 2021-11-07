cd6b2bd8627b980a93f47584ab241996089e29f4
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 15:49:13 -0300

    Create  Leia_vários_números.py

---
+++ TheHuxley/4 - While/Leia_vários_números.py
@@ -0,0 +1,5 @@
+'''Escreva um programa que leia números inteiros da entrada padrão até que seja informado um número negativo. 
+A cada leitura o número lido deve ser escrito na saída padrão.'''
+
+
+# Não fiz

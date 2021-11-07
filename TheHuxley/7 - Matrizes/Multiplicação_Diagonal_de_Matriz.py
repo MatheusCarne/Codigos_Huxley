3627fc0ccc4f6ebf1a7ebdb68b8c77a9ab92fe4a
Matheus Carneiro da Cunha <88046644+MatheusCarne@users.noreply.github.com>
2021-11-07 16:14:33 -0300

    Create  Multiplicação_Diagonal_de_Matriz.py

---
+++ TheHuxley/7 - Matrizes/Multiplicação_Diagonal_de_Matriz.py
@@ -0,0 +1,6 @@
+'''Na matemática é comum o uso de matrizes de números. 
+Estas matrizes podem ser utilizadas para vários fins e a álgebra linear fornece a teoria necessária à sua manipulação.
+Dada uma matriz 4 X 4, você deve fazer um programa para ler uma constante k, ler a matriz e escrevê-la após ter multiplicado os elementos da diagonal principal pela constante k.'''
+
+
+# Não fiz
